6ec6401d915264a2cbbf89e4f3865e6cc7d55782
GitSadiq <sadiqkhanach@gmail.com>
2023-01-05 02:55:19 +0500

    complete cart page functionality

--- src/screens/allcarts/index.jsx
+++ src/screens/allcarts/index.jsx
@@ -1,70 +1,177 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Row, Col } from "antd";
+import { useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 import NavBar from "../../components/navbar";
 import Footer from "../../components/footer";
-import { Row, Col } from "antd";
+import {
+  updatecartdecrement,
+  updatecartincrement,
+  removecart,
+} from "../../store/slices/cartSlice";
 import "../../scss/screens/cart/index.scss";
 
 function Carts() {
+  const navigate = useNavigate();
   const reduxData = useSelector((state) => state);
-  // console.log("cart", reduxData.cartSlice.cartArray)
   const cartArray = reduxData.cartSlice.cartArray;
-  // console.log(cartArray);
+  const dispatch = useDispatch();
+  let total = 0;
+  let shipping = 150;
+
   return (
     <div>
       <NavBar />
-      <div className="cart-body">
-        <h1>Cart</h1>
-        <hr />
-        <Row gutter={[16, 16]} className="row-1">
-          <Col span={4} className="col">
-            <h2>Image</h2>
-          </Col>
-          <Col span={6} className="col">
-            <h2>Title</h2>
-          </Col>
-          <Col span={5} className="col">
-            <h2>Quantity</h2>
-          </Col>
-          <Col span={4} className="col">
-            <h2>price</h2>
-          </Col>
-          <Col span={4} className="col">
-            <h2>Action</h2>
-          </Col>
-        </Row>
-        <hr />
+      {cartArray[0] ? (
+        <div className="cart-body">
+          <h1>Cart</h1>
+          <hr />
+          <Row gutter={[16, 16]} className="row-1">
+            <Col span={4} className="col">
+              <h2>Image</h2>
+            </Col>
+            <Col span={6} className="col">
+              <h2>Title</h2>
+            </Col>
+            <Col span={5} className="col">
+              <h2>Quantity</h2>
+            </Col>
+            <Col span={4} className="col">
+              <h2>price</h2>
+            </Col>
+            <Col span={4} className="col">
+              <h2>Action</h2>
+            </Col>
+          </Row>
+          <hr />
 
-        {cartArray.map((item, index) => {
-          return (
-            <Row key={index} gutter={[16, 16]} className="row-2">
-              <Col span={4} className="col">
-                <img
-                  src={item.url[0]}
-                  alt="no found"
-                />
-              </Col>
-              <Col span={6} className="col">
-                <p>{item.title}</p>
-              </Col>
-              <Col span={5} className="col">
-                <div className="quantity-div">
-                  <button>+</button>
-                  <p>{item.quantity}</p>
-                  <button>-</button>
-                </div>
-              </Col>
-              <Col span={4} className="col">
-                <p>{item.updatedprice}</p>
-                <p>1 x {item.price}</p>
-              </Col>
-              <Col span={4} className="col">
-                <p className="remove">Remove</p>
+          {cartArray.map((item, index) => {
+            total = total + item.updatedprice;
+            console.log(total);
+            return (
+              <Row key={index} gutter={[16, 16]} className="row-2">
+                <Col span={4} className="col">
+                  <img src={item.url[0]} alt="no found" />
+                </Col>
+                <Col span={6} className="col">
+                  <p>{item.title}</p>
+                </Col>
+                <Col span={5} className="col">
+                  <div className="quantity-div">
+                    <button
+                      onClick={() => dispatch(updatecartdecrement(index))}
+                    >
+                      -
+                    </button>
+                    <p>{item.quantity}</p>
+                    <button
+                      onClick={() => dispatch(updatecartincrement(index))}
+                    >
+                      +
+                    </button>
+                  </div>
+                </Col>
+                <Col span={4} className="col">
+                  <p>{item.updatedprice}</p>
+                  <p>1 x {item.price}</p>
+                </Col>
+                <Col span={4} className="col">
+                  <p
+                    className="remove"
+                    onClick={() => {
+                      swal({
+                        title: "Are you sure?",
+                        text: "This item will be deleted from your cart!",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                      }).then((willDelete) => {
+                        if (willDelete) {
+                          dispatch(removecart(index))
+                          swal("Your item has been deleted!", {
+                            icon: "success",
+                          });
+                        } else {
+                          swal("Your item is available in the cart");
+                        }
+                      });
+                    }}
+                  >
+                    Remove
+                  </p>
+                </Col>
+              </Row>
+            );
+          })}
+          <Row gutter={[16, 16]} className="row-3">
+            <Col span={24} className="col">
+              <p
+                className="remove"
+                onClick={() => {
+                  swal({
+                    title: "Are you sure?",
+                    text: "This item will be deleted from your cart!",
+                    icon: "warning",
+                    buttons: true,
+                    dangerMode: true,
+                  }).then((willDelete) => {
+                    if (willDelete) {
+                      dispatch(removecart())
+                      swal("Your all items have been deleted! Now your cart is empty", {
+                        icon: "success",
+                      });
+                    } else {
+                      swal("Your all items are available in the cart!");
+                    }
+                  });
+                }}
+              >
+                Remove all Carts
+              </p>
+            </Col>
+          </Row>
+          <hr />
+          <Row gutter={[16, 16]} className="row-4">
+            <Col span={24} className="col">
+              <h2>Order Summary</h2>
+            </Col>
+          </Row>
+          <hr />
+          <Row gutter={[16, 16]} className="row-5">
+            <Col span={12} className="col">
+              <p>SubTotal</p>
+            </Col>
+            <Col span={12} className="col">
+              <p>RS: {total}</p>
+            </Col>
+            <Col span={12} className="col">
+              <p>Shipping Fee</p>
+            </Col>
+            <Col span={12} className="col">
+              <p>RS: {shipping} </p>
+            </Col>
+
+            <Col span={12} className="col">
+              <h2>Total</h2>
+            </Col>
+            <Col span={12} className="col">
+              <h2>RS: {shipping + total} </h2>
+            </Col>
+            <Row gutter={[16, 16]} className="row-6">
+              <Col span={24} className="col">
+                <button>Proceed to Checkout</button>
               </Col>
             </Row>
-          );
-        })}
-      </div>
+          </Row>
+        </div>
+      ) : (
+        <div className="empty-cart">
+          <p>There are no items in this cart</p>
+          <button onClick={() => navigate("/")}>Continue Shopping</button>
+        </div>
+      )}
+
       <Footer />
     </div>
   );
--- src/store/slices/cartSlice.js
+++ src/store/slices/cartSlice.js
@@ -1,20 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
+import swal from "sweetalert";
 const initialState = {
-    cartArray: [],
-}
+  cartArray: [],
+};
+let flag = false;
 const cartSlice = createSlice({
-    name: "cart",
-    initialState,
-    reducers: {
-        addcart: (state, action) => {
-            state.cartArray.push(action.payload)
-            console.log(action)
-        },
-        removecart: (state, action) => {
-            state.cartArray = []
-        }
-    }
-})
+  name: "cart",
+  initialState,
+  reducers: {
+    addcart: (state, action) => {
+      state.cartArray.push(action.payload);
+      console.log(action);
+    },
+    updatecartincrement: (state, action) => {
+      state.cartArray[action.payload].quantity++;
+      state.cartArray[action.payload].updatedprice =
+        state.cartArray[action.payload].price *
+        state.cartArray[action.payload].quantity;
+    },
+    updatecartdecrement: (state, action) => {
+      if (state.cartArray[action.payload].quantity > 1) {
+        state.cartArray[action.payload].quantity--;
+        state.cartArray[action.payload].updatedprice =
+          state.cartArray[action.payload].price *
+          state.cartArray[action.payload].quantity;
+      } else {
+        swal("Alert!", "You can't reduce the quantity from one!", "warning");
+      }
+    },
+    removecart: (state, action) => {
+      if (action.payload >= 0) {
+        state.cartArray.splice(action.payload, 1);
+      } else {
+        state.cartArray = [];
+      }
+    },
+  },
+});
 
-export const {addcart, removecart} = cartSlice.actions
-export default cartSlice.reducer
+export const { addcart, removecart, updatecartincrement, updatecartdecrement } =
+  cartSlice.actions;
+export default cartSlice.reducer;
